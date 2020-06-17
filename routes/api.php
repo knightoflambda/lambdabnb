f4951975d2f7fea75526a54726581e18b0d18def
Aaron <deograciasaaron@gmail.com>
2020-06-17 20:10:20 +0800

    changed requests to apiresource

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('bookables', 'Api\BookableController@index');
-Route::get('bookables/{id}', 'Api\BookableController@show');
+Route::apiResource('bookables', 'Api\BookableController');
