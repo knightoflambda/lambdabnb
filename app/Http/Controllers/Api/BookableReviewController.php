b4ff5f5d8441f7b5f292af31073878233904bda5
Aaron <deograciasaaron@gmail.com>
2020-06-26 17:33:44 +0800

    created API resource controller for reviews

--- app/Http/Controllers/Api/BookableReviewController.php
+++ app/Http/Controllers/Api/BookableReviewController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Bookable;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BookableIndexResource;
+use App\Http\Resources\BookableReviewIndexResource;
 use Illuminate\Http\Request;
 
 class BookableReviewController extends Controller
@@ -13,8 +16,11 @@ class BookableReviewController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke($id, Request $request)
     {
-        //
+        $bookable = Bookable::findOrFail($id);
+        return BookableReviewIndexResource::collection(
+            $bookable->reviews()->latest()->get()
+        );
     }
 }
